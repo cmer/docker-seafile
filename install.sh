474d32693d21e7a5d83ad5c5b5a0ad4079fd9cb7
Carl Mercier <carl@carlmercier.com>
2017-05-29 12:59:40 -0400

    Added missing libmemcached-dev dependency

--- install.sh
+++ install.sh
@@ -2,8 +2,9 @@ DEBIAN_FRONTEND=noninteractive apt-get update \
   && apt-get install -y python2.7 libpython2.7 python-mysqldb \
       python-setuptools python-pip python-imaging python-ldap \
       python-urllib3 sqlite3 python-memcache curl ack-grep libnss3 \
-      openjdk-8-jre poppler-utils ffmpeg \
+      openjdk-8-jre poppler-utils ffmpeg libmemcached-dev \
   && apt-get clean && rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/* \
+  && pip install --upgrade pip \
   && pip install pillow moviepy pylibmc django-pylibmc
 
 useradd -d /seafile -M -s /bin/bash -c "Seafile User" seafile
