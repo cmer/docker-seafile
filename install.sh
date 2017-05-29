dcab19d5ebc551672dfa6acf6dbf3f3097e8745f
Carl Mercier <carl@carlmercier.com>
2017-05-29 14:15:13 -0400

    Added dependencies for video thumbnails

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
 DEBIAN_FRONTEND=noninteractive apt-get update \
-  && apt-get install -y python2.7 libpython2.7 python-mysqldb \
-      python-setuptools python-pip python-imaging python-ldap \
+  && apt-get install -y locales python2.7 libpython2.7 python-mysqldb \
+      python-setuptools python-dev python-pip python-imaging python-ldap \
       python-urllib3 sqlite3 python-memcache curl ack-grep libnss3 \
-      openjdk-8-jre poppler-utils \
-  && apt-get clean && rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
-
-# RUN pip install pylibmc django-pylibmc
+      openjdk-8-jre poppler-utils ffmpeg libmemcached-dev libz-dev \
+  && apt-get clean && rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/* \
+  && pip install --upgrade pip \
+  && pip install pillow moviepy pylibmc django-pylibmc
 
 useradd -d /seafile -M -s /bin/bash -c "Seafile User" seafile
 mkdir -p /opt/haiwen /seafile/
